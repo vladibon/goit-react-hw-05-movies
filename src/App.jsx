ad574bd94d7b7f0d3c3f332d9426eb3f5ec3e3d3
Vladimir Bondar <vladi.bondar@gmail.com>
2021-11-27 16:46:12 +0200

    fix lazy routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import AppBar from 'components/AppBar';
 import Container from 'components/Container';
 import Loading from 'components/Loading/Loading';
+import Cast from 'components/Cast';
+import Reviews from 'components/Reviews';
+import Trailer from 'components/Trailer';
 
 const HomePage = lazy(() =>
   import('pages/HomePage' /* webpackChunkName: "home-page" */),
@@ -13,17 +16,12 @@ const MoviesPage = lazy(() =>
 const MovieDetailsPage = lazy(() =>
   import('pages/MovieDetailsPage' /* webpackChunkName: "details-page" */),
 );
-const Cast = lazy(() =>
-  import('components/Cast' /* webpackChunkName: "cast" */),
-);
-const Reviews = lazy(() =>
-  import('components/Reviews' /* webpackChunkName: "reviews" */),
-);
 
 function App() {
   return (
     <>
       <AppBar />
+
       <Container>
         <Suspense fallback={<Loading />}>
           <Routes>
@@ -32,6 +30,7 @@ function App() {
             <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
               <Route path='cast' element={<Cast />} />
               <Route path='reviews' element={<Reviews />} />
+              <Route path=':trailerKey' element={<Trailer />} />
             </Route>
             <Route path='*' element={<Navigate replace to='/' />} />
           </Routes>
