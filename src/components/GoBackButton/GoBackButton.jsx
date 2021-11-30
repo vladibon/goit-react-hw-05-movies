02fda9efb6959f7ca99e43ce5c9b85e4bd79d2c1
Vladimir Bondar <vladi.bondar@gmail.com>
2021-11-30 09:23:30 +0200

    change button to Link

--- src/components/GoBackButton/GoBackButton.jsx
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -1,23 +1,17 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { HiOutlineArrowNarrowLeft as ArrowLeft } from 'react-icons/hi';
 import s from './GoBackButton.module.css';
 
 function GoBackButton() {
-  const navigate = useNavigate();
   const location = useLocation();
-
   const pathname = location.state?.from?.pathname;
   const search = location.state?.from?.search;
 
   return (
-    <button
-      className={s.button}
-      type='button'
-      onClick={() => navigate(pathname ? `${pathname}${search}` : '/')}
-    >
+    <Link to={pathname ? `${pathname}${search}` : '/'} className={s.button}>
       <ArrowLeft className={s.icon} />
       Go back
-    </button>
+    </Link>
   );
 }
 
